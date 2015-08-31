2b43b844b6868af0ef2eeb43ef214963cdfd9a30
Metasov Arthur <metasov@gmail.com>
2015-08-31 13:13:03 +0300

    Add settings_local import

--- skutest/skutest/settings.py
+++ skutest/skutest/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -100,3 +101,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from settings_local import *
+except ImportError:
+    sys.stderr.write("Warning: Cannot import settings_local, using default configuration\n")
