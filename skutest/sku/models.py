ccc3209ac5063c0c3c3c3929eeb74a23264919de
Metasov Arthur <metasov@gmail.com>
2015-08-31 18:41:14 +0300

    Add Brand and Category models

--- skutest/sku/models.py
+++ skutest/sku/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=80, blank=False)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=80, blank=False)
