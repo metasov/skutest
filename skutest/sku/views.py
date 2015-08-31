111416d031ac16cc443d992af0f99b58e2051387
Metasov Arthur <metasov@gmail.com>
2015-09-01 00:55:11 +0300

    Add views for all models

--- skutest/sku/views.py
+++ skutest/sku/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
+from sku.models import (
+    Category,
+    Brand,
+    Item)
+from sku.serializers import (
+    CategorySerializer,
+    BrandSerializer,
+    ItemSerializer)
+from rest_framework import (
+    viewsets,
+    mixins)
+
+
+class CategoryViewSet(
+                      mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
+                      mixins.RetrieveModelMixin,
+                      viewsets.GenericViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class BrandViewSet(
+                   mixins.CreateModelMixin,
+                   mixins.ListModelMixin,
+                   mixins.RetrieveModelMixin,
+                   viewsets.GenericViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+
+class ItemViewSet(
+                  mixins.CreateModelMixin,
+                  mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  viewsets.GenericViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
