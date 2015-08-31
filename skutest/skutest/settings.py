848adade50fc632ce7a982b071f812fd331a828a
Metasov Arthur <metasov@gmail.com>
2015-08-31 18:03:35 +0300

    Remove SECRET_KEY from settings.py, move it to settings_local.py

--- skutest/skutest/settings.py
+++ skutest/skutest/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dl-wz%l34(a*1idyo1k7ds54@e21xnb&^#pn&u(d4+*1s14xp1'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
